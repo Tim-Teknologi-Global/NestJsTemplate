191909750986c984372536cbd5d4828aaa72c845
flitzcore_work <hemistergt@gmail.com>
2025-04-08 16:32:47 +0700

    Add docs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,26 +13,42 @@ import { User } from './schemas/user.schema';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+  /**
+   * Register a new user
+   * @param body {email: string, password: string, username?: string}
+   * @returns {access_token: string, refresh_token: string}
+   */
   @Post('register')
   async register(
     @Body() body: { email: string; password: string; username?: string },
   ) {
     return this.authService.register(body.email, body.password, body.username);
   }
-
+  /**
+   * Login a user
+   * @param body {email: string, password: string}
+   * @returns {access_token: string, refresh_token: string}
+   */
   @Post('login')
   async login(@Body() body: { email: string; password: string }) {
     return this.authService.login(body.email, body.password);
   }
-
+  /**
+   * Logout a user
+   * @param request {Request}
+   * @returns {message: string}
+   */
   @Post('logout')
   @UseGuards(JwtAuthGuard)
   async logout(@Req() request: Request) {
     const user = request.user as User;
     return this.authService.logout(user._id);
   }
-
+  /**
+   * Refresh a user's token
+   * @param body {refresh_token: string}
+   * @returns {access_token: string, refresh_token: string}
+   */
   @Post('refresh')
   async refreshToken(@Body() body: { refresh_token: string }) {
     return this.authService.refreshToken(body.refresh_token);
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -14,33 +14,55 @@ import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { Request } from 'express';
-
+import { User } from '../auth/schemas/user.schema';
 @Controller('items')
 @UseGuards(JwtAuthGuard)
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
-
+  /**
+   * Create a new item
+   * @param createItemDto {CreateItemDto}
+   * @returns {Item}
+   */
   @Post()
   create(@Body() createItemDto: CreateItemDto) {
     return this.itemsService.create(createItemDto);
   }
-
+  /**
+   * Get all items
+   * @param request {Request}
+   * @returns {Item[]}
+   */
   @Get()
   findAll(@Req() request: Request) {
-    console.log(request.headers);
+    // this is how you get the user from the request
+    const user = request.user as User;
     return this.itemsService.findAll();
   }
-
+  /**
+   * Get a single item by ID
+   * @param id {string}
+   * @returns {Item}
+   */
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.itemsService.findOne(id);
   }
-
+  /**
+   * Update a single item by ID
+   * @param id {string}
+   * @param updateItemDto {UpdateItemDto}
+   * @returns {Item}
+   */
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateItemDto: UpdateItemDto) {
     return this.itemsService.update(id, updateItemDto);
   }
-
+  /**
+   * Delete a single item by ID
+   * @param id {string}
+   * @returns {Item}
+   */
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.itemsService.remove(id);
